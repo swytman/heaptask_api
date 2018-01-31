51cae11508336361c7b0e8d7e92fe1f647401ed3
swyt <swytman@gmail.com>
2018-02-01 00:28:18 +0300

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'authenticate', to: 'authentication#authenticate'
 
   resources :tasks
+  resources :users, only: [:create]
   get 'token', to: 'master#token'
   root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
