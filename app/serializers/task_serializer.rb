292377091dd7a6922799b9a442f904ca45fd09ac
VS <vmstepanov@dasreda.ru>
2018-04-22 21:56:25 +0300

    fix bug

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -2,10 +2,10 @@ class TaskSerializer < ActiveModel::Serializer
   attributes :id, :start, :end, :description, :name, :importance, :urgency
 
   def start
-    object.start.strftime('%Y-%m-%d')
+    object.start&.strftime('%Y-%m-%d')
   end
 
   def end
-    object.end.strftime('%Y-%m-%d')
+    object.end&.strftime('%Y-%m-%d')
   end
 end
