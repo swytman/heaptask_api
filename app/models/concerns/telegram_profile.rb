23ec57817d5f477b94d185872ca4ce87fd98f2a4
VS <vmstepanov@dasreda.ru>
2018-08-04 22:46:42 +0300

    fix

--- app/models/concerns/telegram_profile.rb
+++ app/models/concerns/telegram_profile.rb
@@ -1,10 +1,12 @@
 module TelegramProfile
   extend ActiveSupport::Concern
 
-  jsonb_accessor :telegram_bot_data,
-                 name: :string,
-                 price: [:integer, store_key: :p],
-                 price_in_cents: :integer,
-                 reviewed_at: :datetime
+  included do
+    jsonb_accessor :telegram_bot_data,
+                   name: :string,
+                   price: [:integer, store_key: :p],
+                   price_in_cents: :integer,
+                   reviewed_at: :datetime
+  end
 end
 
